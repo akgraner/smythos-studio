3429a9a7abdd9134906ba19003119790779a50e1
forhad-hosain <103570062+forhad-hosain@users.noreply.github.com>
2025-09-03 06:16:31 +0600

    Runtime (#92)
    * add runtime package
    
    * resolve all imports for embodiment
    
    * resolve import for agent-runner
    
    * fix embodiment routes
    
    * offload index.ts
    
    * update entry file name for runtime server
    
    * name Agent Data connector as SmythOSSAgentData
    
    * bring back the logic to serve custom models
    
    * apply standard order in module imports
    
    * remove the implementation of adapting model based on the planInfo
    
    * remove planInfo footprint
    
    * adjust constants and remove agent.helper from embodiment
    
    * set default model to gpt-5-mini
    
    * add "Echo" model
    
    * move models file to "data"
    
    * rename openai router file
    
    * update connectorRegistry.ts file name
    
    * add code components and use JSON file vault
    
    * add code sandbox to the package
    
    * add `ses` to run the code sandbox
    
    * clean env unnecessary variables
    
    * make hardcoded port consistent and adjust env variable names
    
    * get env from config where it make sense
    
    * run agent with localhost
    
    * reflect the the .env to .env.example

---
+++ packages/runtime/src/core/connectors/connectorRegistry.ts
@@ -0,0 +1,29 @@
+import {
+  ConnectorService,
+  JSONModelsProvider,
+  TConnectorService,
+} from "@smythos/sre";
+
+import { SmythOSSAccount } from "./SmythOSSAccount.class";
+import { SmythOSSAgentDataConnector } from "./SmythOSSAgentDataConnector.class";
+import { SmythOSSVault } from "./SmythOSSVault.class";
+
+const CONNECTORS = [
+  [
+    TConnectorService.AgentData,
+    "SmythOSSAgentData",
+    SmythOSSAgentDataConnector,
+  ],
+  [TConnectorService.Account, "SmythOSSAccount", SmythOSSAccount],
+  [TConnectorService.Vault, "SmythOSSVault", SmythOSSVault],
+  [TConnectorService.ModelsProvider, "SmythModelsProvider", JSONModelsProvider],
+] as const;
+
+/**
+ * Register all connectors
+ */
+export function registerConnectors(): void {
+  CONNECTORS.forEach(([serviceType, name, connectorClass]) => {
+    ConnectorService.register(serviceType, name, connectorClass);
+  });
+}
