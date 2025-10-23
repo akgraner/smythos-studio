cb5670fe2a7367c6c05127b0a817911113a2a49f
Forhad Hosain <forhad@smythos.com>
2025-10-23 22:03:40 +0600

    remove keyOptions implementation of model template from code

--- packages/app/src/backend/services/LLMHelper/index.ts
+++ packages/app/src/backend/services/LLMHelper/index.ts
@@ -7,6 +7,7 @@ import { getTeamSettingsObj } from '../../services/team-data.service';
 import { vault } from '../SmythVault.class';
 import { LLMService } from './LLMService.class';
 
+// TODO: Refactor this function to be more readable and maintainable.
 async function getUserLLMModels(req: Request) {
   const models = {};
   try {
@@ -27,22 +28,18 @@ async function getUserLLMModels(req: Request) {
         modelTpl = { ...modelTpl, ...aliasOptions }; //override the model config with the alias config
       }
 
-      // get the key options
-      const modelKeyOptions = modelTpl.keyOptions || {};
-      delete modelTpl.keyOptions;
-
-      const provider = modelTpl?.provider?.toLowerCase();
-      const hasAPIKey = !!keys[provider?.toLowerCase()];
-
-      if (hasAPIKey) {
-        modelTpl = { ...modelTpl, ...modelKeyOptions }; //override the model config with the key config
-        modelTpl.hasKey = true;
-      }
-
       let enabled =
         typeof LLMModels[modelId].enabled === 'function'
           ? LLMModels[modelId].enabled({ user: req.user, team: req._team })
           : modelTpl.enabled;
+
+      const provider = modelTpl?.provider?.toLowerCase();
+      const hasAPIKey = !!keys[provider?.toLowerCase()];
+
+      // If the credentials are only 'vault' and the team doesn't have their own API key, disable the model.
+      if (modelTpl.credentials === 'vault' && !hasAPIKey) {
+        enabled = false;
+      }
 
       // Skip legacy models for users with built-in model access to avoid confusion
       if (_hasBuiltInModels(req) && modelId.startsWith('legacy/')) {
